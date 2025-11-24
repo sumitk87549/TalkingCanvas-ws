1a0834e9a3fb3096c04ef091bb3df81443f35a41
Sumit Kumar <sumitk87549@gmail.com>
2025-11-24 20:11:30 +0530

    reset

--- client/src/app/features/admin/admin.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const ADMIN_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./admin-dashboard/admin-dashboard.component').then(m => m.AdminDashboardComponent)
-  }
-];
--- client/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  // apiUrl: 'http://localhost:8081/api'
-  // apiUrl: 'http://localhost:8081/api'
-  apiUrl: 'http://localhost:8082/api'
-};
